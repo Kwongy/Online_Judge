c52712f983c067e89bce563bbd92abaa56212537
Kwongy <760030764@qq.com>
2019-05-27 10:53:20 +0800

    逻辑

---
+++ leetcode/860.py
@@ -0,0 +1,25 @@
+class Solution:
+    def lemonadeChange(self, bills: List[int]) -> bool:
+        five, ten = 0, 0
+        for i in bills:
+            if i == 5:
+                five += 1
+            elif i == 10:
+                if five > 0:
+                    ten += 1
+                    five -= 1
+                else:
+                    return False
+            else:
+                if ten > 0:
+                    if five > 0:
+                        ten -= 1
+                        five -= 1
+                    else: 
+                        return False
+                else:
+                    if five > 2:
+                        five -= 3
+                    else:
+                        return False
+        return True
